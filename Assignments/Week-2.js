7d8b6e78439c5ac3fdfc7deed8cc3048fd38d09c
unknown <JacobPearsonPro@gmail.com>
2021-09-05 14:00:09 -0500

    move week-2

---
+++ Assignments/Week-2.js
@@ -0,0 +1,13 @@
+let pizzaPlace = "Papa Murphys";
+let numberOfToppings = 26;
+
+console.log(
+  pizzaPlace,
+  typeof pizzaPlace,
+  numberOfToppings,
+  typeof numberOfToppings
+);
+console.log(`"${pizzaPlace} is a wonderful way to get dinner."`);
+numberOfToppings < 10
+  ? console.log("Quality, not quantity.")
+  : console.log("A whole lot of pizza.");
