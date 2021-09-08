a45c34044e5a35d870c7c29cfc956e123b37d2fc
unknown <JacobPearsonPro@gmail.com>
2021-09-07 19:32:04 -0500

    3.1.1 and 3.1.2 activities

---
+++ ClassActivities/3.1.1-FunctionDeclaration.js
@@ -0,0 +1,18 @@
+function greet() {
+  console.log("Hello!");
+};
+
+function threeModTwo() {
+  console.log(3 % 2);
+};
+
+function checkDrivingAge(age) {
+  if (age >= 16) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+};
+greet()
+threeModTwo()
+checkDrivingAge(17)
