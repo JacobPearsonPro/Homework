dcb709f9bd6e321833bff95aa1f2b6bf08220593
unknown <JacobPearsonPro@gmail.com>
2021-09-07 19:43:19 -0500

    add console.log(console.log())

--- ClassActivities/3.1.2-FunctionInvocation.js
+++ ClassActivities/3.1.2-FunctionInvocation.js
@@ -10,4 +10,4 @@ function myNumberFunction() {
   // now, invoke the myNumberFunction function
   myNumberFunction()
   // print just the function of console.log (no invocation)
-  myNumberFunction
+console.log(console.log())
